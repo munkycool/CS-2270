43b64ec9b13fa38f082242e0fe6112f3ccaff9d8
Artemis Tack <artemis@art3mix.com>
2024-10-04 20:11:57 -0500

    Updates to class examples

--- Class examples/2024-10-02/untitled/src/loopyLoops.java
+++ Class examples/2024-10-02/untitled/src/loopyLoops.java
@@ -2,18 +2,48 @@ import java.util.Scanner;
 
 public class loopyLoops {
     void main() {
-        Scanner scnr = new Scanner(System.in);
+//        Scanner scnr = new Scanner(System.in);
+//
+//        while (true) {
+//            System.out.println("Continue? (y or n): ");
+//            String userInput = scnr.next();
+//
+//            if (userInput.equals("y")) {
+//                System.out.println("Continuing ...");
+//            } else {
+//                System.out.println("Exiting ...");
+//                break;
+//            }
+//        }
+//        System.out.println("Bye-bye now...");
 
-        System.out.println("Continue? (y or n): ");
-        String userInput = scnr.next();
+        //countToTen();
+        //liftOff();
+        int days = howLongCanIEat(300);
+        System.out.println("You can eat for " + days + " days.");
+    }
 
-        if (userInput.equals("y")) {
-            System.out.println("Continuing ...");
+    void countToTen() {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
         }
-        else {
-            System.out.println("Exiting ...");
+    }
+
+    void liftOff() {
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+
+    int howLongCanIEat(double m) {
+        int days = 0;
+
+        while (m >= 20) {
+            m -= 20;
+            m *= 0.90;
+            days++;
         }
 
-        System.out.println("Bye-bye now...");
+        return days;
     }
 }
